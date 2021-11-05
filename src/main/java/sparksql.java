14a60d4e9f5c40bc6419ebf337c58aba646a202a
haordeng <haordeng@cisco.com>
2021-11-05 13:34:12 +0800

    .

--- src/main/java/sparksql.java
+++ src/main/java/sparksql.java
@@ -1,2 +1,23 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+
 public class sparksql {
+
+    public static void main(String []args){
+
+        System.setProperty("HADOOP_USER_NAME","root");
+        SparkConf sparkConf = new SparkConf().setAppName("Java Spark Hive Example").setMaster("local[2]");
+
+        SparkSession spark = SparkSession
+                .builder()
+                .config(sparkConf)
+                .enableHiveSupport()
+                .getOrCreate();
+        spark.sql("show databases").show();
+
+        spark.sql("select * from bigking.house_price");
+
+        spark.stop();
+
+    }
 }
